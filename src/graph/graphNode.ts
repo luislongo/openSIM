1021310d2a559d82f675806dc631a66b18738db0
luislongo <luis.longof@gmail.com>
2022-08-04 18:50:29 -0300

    Creates a base Graph class

---
+++ src/graph/graphNode.ts
@@ -0,0 +1,24 @@
+import { v4 as uuidv4 } from "uuid";
+
+export class GraphNode {
+  uuid: string;
+  from: GraphNode[];
+  to: GraphNode[];
+  value: number;
+
+  constructor(value) {
+    this.uuid = uuidv4();
+    this.from = [];
+    this.to = [];
+    this.value = value;
+  }
+
+  addChild(child: GraphNode) {
+    this.to.push(child);
+    child.from.push(this);
+  }
+  removeChild(child) {
+    this.to = this.to.filter((c) => c.value !== child.value);
+    child.from = child.from.filter((c) => c.value !== this.value);
+  }
+}
