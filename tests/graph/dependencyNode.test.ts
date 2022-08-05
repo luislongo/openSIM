56baf1f1efd9fbfb28bddf7f61f1046d3d911a36
luislongo <luis.longof@gmail.com>
2022-08-04 22:00:05 -0300

    Creates tests to validate propagation behaviour

--- tests/graph/dependencyNode.test.ts
+++ tests/graph/dependencyNode.test.ts
@@ -1,18 +1,27 @@
-import { DependencyNode } from "src/graph/dependencyNode.class";
+import { DependencyNode } from "../../src/graph/dependencyNode.class";
 
-describe("DependencyNode behaviour", () => {
-  it("should update children nodes", () => {
+describe("DependencyNodeNode behaviour", () => {
+  it("Should not add unrelated nodes", () => {
     const nodeA = new DependencyNode(1);
-    const nodeB = new DependencyNode(2);
-    const nodeC = new DependencyNode(3);
-    const nodeD = new DependencyNode(4);
+    const nodeA_A = new DependencyNode(2);
+    const nodeA_B = new DependencyNode(3);
+    const nodeA_A_A = new DependencyNode(4);
+    const nodeA_A_B = new DependencyNode(5);
+    const nodeA_B_A = new DependencyNode(6);
 
-    nodeA.addChild(nodeB);
-    nodeA.addChild(nodeC);
-    nodeB.addChild(nodeD);
+    nodeA.addChild(nodeA_A);
+    nodeA.addChild(nodeA_B);
+    nodeA_A.addChild(nodeA_A_A);
+    nodeA_A.addChild(nodeA_A_B);
+    nodeA_B.addChild(nodeA_B_A);
 
-    expect(nodeB.value.current).toBe(10);
-    expect(nodeC.value.current).toBe(10);
-    expect(nodeD.value.current).toBe(10);
+    nodeA.set(2);
+
+    expect(nodeA.value.current).toBe(2);
+    expect(nodeA_A.value.current).toBe(4);
+    expect(nodeA_B.value.current).toBe(4);
+    expect(nodeA_A_A.value.current).toBe(8);
+    expect(nodeA_A_B.value.current).toBe(8);
+    expect(nodeA_B_A.value.current).toBe(8);
   });
 });
